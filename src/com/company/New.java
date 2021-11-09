6d3aeeff4e2c51cea47c9e3b4a60deeb4a4ae9c7
Denis <denis.vishchak@gmail.com>
2021-11-09 20:59:52 +0200

    added get/set;

--- src/com/company/New.java
+++ src/com/company/New.java
@@ -8,5 +8,19 @@ public class New {
         this.name = name;
     }
 
+    public Long getAge() {
+        return age;
+    }
 
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
