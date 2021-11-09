74001da0a12bfeabf73201843574e03b3dd8fd4c
Denis <denis.vishchak@gmail.com>
2021-11-09 20:34:40 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter file1 = new FileWriter("test1.txt",true);
+            PrintWriter pw = new PrintWriter(file1);
+            for (int i = 0; i <10 ; i++) {
+                pw.println("hello world"+i);
+
+            }
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
